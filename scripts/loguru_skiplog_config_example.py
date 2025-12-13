5aaddc46a9c19f38b7babab14737628b9ffd4923
Henning Thieß <ht@xomox.cc>
2025-12-13 18:50:06 +0100

    Add example script for applying `loguru` patch with skiplog filter
    [skip actions]

---
+++ scripts/loguru_skiplog_config_example.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+from loguru import logger
+import reputils.MailReport
+
+def mrsendmail_apply_loguru_patch(skiplog: bool = False) -> None:
+    logger.info(f"mrsendmail_apply_loguru_patch({skiplog=})")
+    # print("\n", file=sys.stderr, flush=True, end="")
+    @classmethod
+    def logtest(cls):
+        melo = logger.bind(classname=cls.__qualname__)
+        with melo.contextualize(skiplog=skiplog):
+            # logger = logger.bind(skiplog=skiplog)
+            melo.info(f"{__name__=} {skiplog=}")
+            melo.debug(f"{__name__=} {skiplog=}")
+
+            # if not skiplog:
+            #     print("\n", file=sys.stderr, flush=True, end="")
+
+            # if "__qualname__" in vars():
+            melo.info(f"{cls.__qualname__=}")
+            melo.debug(f"{cls.__qualname__=}")
+
+            # if not skiplog:
+            #     print("\n", file=sys.stderr, flush=True, end="")
+
+    # patch
+    reputils.MailReport.MRSendmail.logtest = logtest
+
+
+def main():
+    logger.info(f"in main()::{__name__=} :: before configuring loguru")
+
+    reputils.MailReport.configure_loguru_default_with_skiplog_filter()
+
+    for t in [False, True]:
+        mrsendmail_apply_loguru_patch(skiplog=t)
+        logger.info(f"reputils.MailReport.MRSendmail.logtest(skiplog={t}) -> start")
+        # print("\n", file=sys.stderr, flush=True, end="")
+        reputils.MailReport.MRSendmail.logtest()
+        # print("\n", file=sys.stderr, flush=True, end="")
+        logger.info(f"reputils.MailReport.MRSendmail.logtest(skiplog={t}) -> done")
+        # print("\n", file=sys.stderr, flush=True, end="")
+
+
+if __name__ == "__main__":
+    logger.info("INFO-LEVEL (DEBUG-LEVEL FOLLOWS) before main.")
+    logger.debug("DEBUG-LEVEL (INFO-LEVEL was before) before main.")
+    main()
+    logger.info("INFO-LEVEL only after main.")
